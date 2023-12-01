57f13756863ed1958174124c3fbd79e42f707f4b
hinken <phnome@protonmail.com>
2023-12-01 15:22:06 +0100

    git init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut output = 0;
+
+    for x in include_str!("input_day1_1.txt").lines() {
+        let number: String = x.chars().filter(|x| x.is_ascii_digit()).collect();
+        output += (number.chars().next().unwrap() as u32 - '0' as u32) * 10
+            + (number.chars().last().unwrap() as u32 - '0' as u32);
+    }
+
+    println!("{}", output);
+}
